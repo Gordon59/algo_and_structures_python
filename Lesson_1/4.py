3e36573047c58bee3afc60055996682cb8690e6d
Gordon59 <almazsafin2003@gmail.com>
2019-03-09 10:20:40 +0500

    а

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,19 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+import random as r
+
+gr1 = int(input('Граница 1: \n'))
+gr2 = int(input('Граница 2: \n'))
+alf = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+dict= {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25}
+print(dict.get('c'))
+print(r.randint(gr1, gr2))
+print(r.uniform(gr1, gr2))
+
+gr3 = input('C буквы:')
+gr4 = input('До буквы:')
+
+gr3 = dict.get(gr3)
+gr4 = dict.get(gr4)
+#print(r.choices(alf(gr3,gr4)))
--- Lesson_1/9.py
+++ Lesson_1/9.py
@@ -1,3 +1,19 @@
 # 9.Вводятся три разных числа. Найти, какое из них
 # является средним (больше одного, но меньше другого).
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+if num1 > num2 and num1 < num3:
+    print('1 число')
+elif num1 < num2 and num1 > num3:
+    print('1 число')
+elif num2 > num1 and num2 < num3:
+    print('2 число')
+elif num2 < num1 and num2 > num3:
+    print('2 число')
+elif num3 > num1 and num3 < num2:
+    print('3 число')
+elif num3 < num1 and num3 > num2:
+    print('3 число')
 
